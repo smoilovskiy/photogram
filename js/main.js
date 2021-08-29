807bbfc8ba4d2ffbd222736976d5df0c6a3f607d
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-08-29 23:14:39 +0300

    add comments array

--- js/main.js
+++ js/main.js
@@ -4,15 +4,26 @@ import { getRandomInt } from './randomGen.js';
 const OBJECTS_COUNT = 25;
 const COMMENTS_COUNT = 100;
 const DESCRIPTIONS = ['description1', 'description2', 'description3', 'description4',
- 'description5', 'description6', 'description7', 'description8', 'description9', 
- 'description10', 'description11', 'description12', 'description13', 'description14', 
- 'description15', 'description16', 'description17', 'description18', 'description19', 
- 'description20', 'description21', 'description22', 'description23', 'description24', 
- 'description25'];
+  'description5', 'description6', 'description7', 'description8', 'description9',
+  'description10', 'description11', 'description12', 'description13', 'description14',
+  'description15', 'description16', 'description17', 'description18', 'description19',
+  'description20', 'description21', 'description22', 'description23', 'description24',
+  'description25'];
 const LIKES_MIN = 15;
 const LIKES_MAX = 200;
 const AVATAR_MIN = 1;
 const AVATAR_MAX = 6;
+const MESSAGES = [
+  `Всё отлично!`,
+  `В целом всё неплохо. Но не всё.`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
+];
+const MESSAGES_MIN = 0;
+const MESSAGES_MAX = 5;
+const NAMES = ['Артем', 'Степан', 'Василий', 'Валя', 'Маша'];
 let counter = 0;
 let cCounter = 0;
 
@@ -32,11 +43,13 @@ export const photos = new Array(OBJECTS_COUNT).fill().map(function () {
 
 
 const comments = new Array(COMMENTS_COUNT).fill().map(function () {
-  {
+
+  const commentObj = {
     id: cCounter,
     avatar: `img/avatar-${getRandomInt(AVATAR_MIN, AVATAR_MAX)}.svg`,
-    message: 'В целом всё неплохо. Но не всё.',
-    name: 'Артём',
+    message: MESSAGES[getRandomInt(MESSAGES_MIN, MESSAGES_MAX)] + MESSAGES[getRandomInt(MESSAGES_MIN, MESSAGES_MAX)],
+    name: NAMES[getRandomInt(NAMES_MIN, NAMES_MAX)],
   }
+  cCounter++;
+  return commentObj;
 }
-)
