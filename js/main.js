27f06d7a9e3bb2a711b0fdf991c450bdb9f3fd6f
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-09-07 23:44:05 +0300

    added upload.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import { createPreview } from './preview.js';
 import { showBigImage } from './big-picture.js';
 import { photos } from './photosTempGen.js';
-
+import { upload } from './upload.js';
 
 console.log(photos[0]);
+
 for (let i = 0; i < photos.length; i++) {
   createPreview(photos[i]);
 }
 
 showBigImage(photos);
+upload();
---
+++ js/upload.js
@@ -0,0 +1,40 @@
+export function upload() {
+
+  let body = document.querySelector('body');
+  let uploadImg = document.querySelector('#upload-file');
+  let uploadImgPopup = document.querySelector('.img-upload__overlay');
+  // for user photo upload
+  // let uploadImgPreview = document.querySelector('.img-upload__preview').querySelector('img');
+
+  uploadImg.addEventListener('change', function () {
+    let file = uploadImg.files[0];
+
+    let reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      uploadImgPopup.classList.remove('hidden');
+      body.classList.add('modal-open');
+      // for user photo upload
+      // uploadImgPreview.src = reader.result;
+    });
+
+    reader.readAsDataURL(file);
+
+  });
+
+  window.addEventListener('click', function (evt) {
+    if (evt.target.id === 'upload-cancel') {
+      uploadImgPopup.classList.add('hidden');
+      body.classList.remove('modal-open');
+    }
+  });
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      uploadImgPopup.classList.add('hidden');
+      body.classList.remove('modal-open');
+    }
+  });
+
+
+}
