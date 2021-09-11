8791ea72538e37a3fc2dd4881e01847b09f50f9d
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-09-11 11:54:38 +0300

    add custom validation messages to hashtag input field

--- js/validation.js
+++ js/validation.js
@@ -1,34 +1,39 @@
 export function validation() {
 
   let hashtagsInput = document.querySelector('.text__hashtags');
-  hashtagsInput.setAttribute("minlength", "30");
-  hashtagsInput.setAttribute("maxlength", "100");
 
   hashtagsInput.addEventListener("change", function () {
     let hashtagsLowercase = hashtagsInput.value.toLowerCase();
     let hashtagsArray = hashtagsLowercase.split(' ');
-    let charSet = /^[а-яА-ЯёЁa-zA-Z0-9]+$; //'abcdefghijklmnopqrstuvwxyz'
+
 
     if (hashtagsArray.length > 5) {
       console.log('Too many hashtags. Must be up to 5');
+      hashtagsInput.setCustomValidity('Too many hashtags. Must be up to 5');
+        return;
     }
 
-    for (let i = 0; i < hashtagsArray.length; i++) {
-      let hashtag = hashtagsArray[i];
-      
-      if (!hashtag.matchAll(charSet)) {
-        console.log('Invalid symbols');
+    for (var i = 0; i < hashtagsArray.length; i++) {
+      if (hashtagsArray[i] === '#' || hashtagsArray[i].length === 1) {
+        hashtagsInput.setCustomValidity('Hashtag must contains more than 1 symbol');
+        break;
+      }
+      if (hashtagsArray[i].length > 20) {
+        hashtagsInput.setCustomValidity('Hashtags length can not be bigger than 20');
+        break;
+      }
+      if (hashtagsArray[i].charAt(0) !== '#') {
+        hashtagsInput.setCustomValidity('Hashtag must starts with #');
+        break;
       }
 
-      for (let j = 0; j < hashtag.length; j++) {
-        if (hashtag[0] !== '#') {
-          console.log('First character must be #')
-          break;
-        } 
+      for (var j = i + 1; j < hashtagsArray.length; j++) {
+        if (hashtagsArray[j] === hashtagsArray[i]) {
+          hashtagsInput.setCustomValidity('Hashtags cannot be repeated');
+          return;
+        }
       }
     }
-
-
 
     console.log(hashtagsArray);
   })
