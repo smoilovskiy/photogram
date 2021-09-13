9968c1268255be73ca3464b118d5c961f2b2bb74
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-09-13 15:08:26 +0300

    fix validation bugs

--- js/validation.js
+++ js/validation.js
@@ -6,37 +6,34 @@ export function validation() {
     let hashtagsLowercase = hashtagsInput.value.toLowerCase();
     let hashtagsArray = hashtagsLowercase.split(' ');
 
-
     if (hashtagsArray.length > 5) {
       console.log('Too many hashtags. Must be up to 5');
       hashtagsInput.setCustomValidity('Too many hashtags. Must be up to 5');
-        return;
+      return;
     }
 
-
-
     for (var i = 0; i < hashtagsArray.length; i++) {
+
       if (hashtagsArray[i] === '#' || hashtagsArray[i].length === 1) {
         hashtagsInput.setCustomValidity('Hashtag must contains more than 1 symbol');
         break;
-      }
-
-      if (hashtagsArray[i].length > 20) {
+      } else if (hashtagsArray[i].length > 20) {
         hashtagsInput.setCustomValidity('Hashtags length can not be bigger than 20');
         break;
-      }
-      
-      if (hashtagsArray[i].charAt(0) !== '#') {
+      } else if (hashtagsArray[i].charAt(0) !== '#') {
         hashtagsInput.setCustomValidity('Hashtag must starts with #');
         break;
+      } else {
+        hashtagsInput.setCustomValidity('');
       }
 
-      for (var j = i + 1; j < hashtagsArray.length; j++) {
+      for (let j = i + 1; j < hashtagsArray.length; j++) {
         if (hashtagsArray[j] === hashtagsArray[i]) {
           hashtagsInput.setCustomValidity('Hashtags cannot be repeated');
           return;
         }
       }
+
     }
 
     console.log(hashtagsArray);
