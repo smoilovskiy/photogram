70dc479574f4873654dff55eac938f56cdd539a6
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-09-19 20:08:48 +0300

    add effects adjustment

--- js/picture-edit.js
+++ js/picture-edit.js
@@ -1,10 +1,13 @@
 let scaleValue = document.querySelector('.scale__control--value');
 let scale = document.querySelector('.img-upload__scale');
+
 let uploadImgPreview = document.querySelector('.img-upload__preview').querySelector('img');
+
 let effect = document.querySelector('.effects__radio');
-let effects = document.querySelectorAll('.effects__radio');
+// let effects = document.querySelectorAll('.effects__radio');
 
 let sliderContainer = document.querySelector('.effect-level__slider');
+let effectSelector = document.querySelector('.img-upload__effects');
 
 
 const MIN_VALUE = 25;
@@ -41,37 +44,87 @@ function changeScaleValue() {
   uploadImgPreview.style.transform = `scale(${currentValue / 100})`;
 }
 
+
+
 export function setEffect() {
-   
+
 
   uploadImgPreview.classList.add(`effects__preview--none`);
+
+
   if (effect) {
 
     noUiSlider.create(sliderContainer, {
-      start: 50,
+      start: 0,
       tooltips: true,
       connect: true,
-        range: {
-            "min": 0,
-            "max": 100
-        }
+      range: {
+        "min": 0,
+        "max": 100
+      }
     });
 
+    effectSelector.addEventListener('click', function (evt) {
 
-      effects.forEach((elem) => {
-      elem.addEventListener('change', function (evt) {
-        let item = evt.target.value;
-        uploadImgPreview.className = `effects__preview--${item}`;
+      if (evt.target.value === 'none') {
+        sliderContainer.noUiSlider.reset();
+        uploadImgPreview.value = 'none';
+        
+        sliderContainer.noUiSlider.on('update', function () {
+          uploadImgPreview.style.filter = 'none';
+        });
 
-        sliderContainer.noUiSlider.on('update', function( values, handle ) {
+      }
+
+      if (evt.target.id === 'effect-chrome') {
+        sliderContainer.noUiSlider.reset();
+        uploadImgPreview.value = 'chrome';
+        sliderContainer.noUiSlider.on('update', function (values) {
           let val = ((values.toString()) / 100).toFixed(2);
-          console.log(val);
           uploadImgPreview.style.filter = `grayscale(${val})`;
+        });
+      }
 
-       });
+      if (evt.target.id === 'effect-sepia') {
+        sliderContainer.noUiSlider.reset();
+        uploadImgPreview.value = 'sepia';
+        sliderContainer.noUiSlider.on('update', function (values) {
+          let val = ((values.toString()) / 100).toFixed(2);
+          uploadImgPreview.style.filter = `sepia(${val})`;
+        });
+      }
 
+      if (evt.target.id === 'effect-marvin') {
+        sliderContainer.noUiSlider.reset();
+        uploadImgPreview.value = 'marvin';
+        sliderContainer.noUiSlider.on('update', function (values) {
+          let val = (values.toString());
+          uploadImgPreview.style.filter = `invert(${val}%)`;
+        });
+      }
 
-      });
-    });
+      if (evt.target.id === 'effect-phobos') {
+        sliderContainer.noUiSlider.reset();
+        uploadImgPreview.value = 'phobos';
+        sliderContainer.noUiSlider.on('update', function (values) {
+          let val = ((values.toString()) / 33).toFixed(1);
+          uploadImgPreview.style.filter = `blur(${val}px)`;
+        });  
+      }
+
+      if (evt.target.id === 'effect-heat') {
+        sliderContainer.noUiSlider.reset();
+        uploadImgPreview.value = 'heat';
+        sliderContainer.noUiSlider.on('update', function (values) {
+          let val = ((values.toString()) / 50 + 1).toFixed(1);
+          console.log(val);
+          uploadImgPreview.style.filter = `brightness(${val})`;
+        });  
+      }
+
+    })
+
   }
+
+
 }
